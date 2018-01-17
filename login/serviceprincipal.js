e9200f6fc0b3fbea6365ce86fba7eb204e71ea67
Paul Sydney <xtrycatchx@gmail.com>
2018-01-17 18:53:34 +0800

    experiments on authentications

--- login/serviceprincipal.js
+++ login/serviceprincipal.js
@@ -2,12 +2,12 @@ const msRestAzure = require('ms-rest-azure');
 
 // do a az ad sp create-for-rbac para ani na params
 const clientIdOrAppId = 'xxx',
-secretOrPassword='yyy',
-domainOrTenantId='zzz';
+    secretOrPassword = 'yyy',
+    domainOrTenantId = 'zzz';
 
-msRestAzure.loginWithServicePrincipalSecret(clientIdOrAppId, secretOrPassword, domainOrTenantId, function(err, credentials) {
+msRestAzure.loginWithServicePrincipalSecret(clientIdOrAppId, secretOrPassword, domainOrTenantId, (err, credentials) => {
     err
-    ? console.log(`error ${err}`)
-    : console.log(`credentials ${JSON.stringify(credentials)}`)
-  
+        ? console.log(`error ${err}`)
+        : console.log(`credentials ${JSON.stringify(credentials)}`)
+
 });
