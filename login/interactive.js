56b419cbf158b6805b655e3753fab58a2bffef3a
Paul Sydney <xtrycatchx@gmail.com>
2018-01-17 18:25:48 +0800

    experiments on authentications

--- login/interactive.js
+++ login/interactive.js
@@ -1,5 +1,5 @@
 const msRestAzure = require('ms-rest-azure');
-msRestAzure.interactiveLogin(function (err, credentials) {
+msRestAzure.interactiveLogin((err, credentials) => {
     err
         ? console.log(`error ${err}`)
         : console.log(`credentials ${credentials}`)
